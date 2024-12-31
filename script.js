fa87fdf0c87dba63db4f76af2d3f9a801d9f47bb
dinokecil-dot <indiradh09@gmail.com>
2024-12-31 21:48:29 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function updateCountdown() {
+    const newyearDate = new Date ( 'january 01, 2025 00:00:00').getTime();
+    const now = new Date().getTime();
+    const distance = newyearDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60 )) / 1000);
+
+    document.getElementById('days').innerHTML = days.toString().padStart(2, '0');
+    document.getElementById('hours').innerHTML = hours.toString().padStart(2, '0');
+    document.getElementById('minutes').innerHTML = minutes.toString().padStart(2, '0');
+    document.getElementById('seconds').innerHTML = seconds.toString().padStart(2, '0');
+}
+
+setInterval(updateCountdown, 1000);
+updateCountdown();
